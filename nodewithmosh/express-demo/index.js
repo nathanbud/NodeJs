7c220196500cd516d433db10dff5461058bfe650
nathanbud <nthnlbdky916@gmail.com>
2019-02-20 23:32:59 +0800

    added PORT

--- nodewithmosh/express-demo/index.js
+++ nodewithmosh/express-demo/index.js
@@ -12,4 +12,6 @@ app.get('/api/courses', (req, res) => {
     res.send([1, 2, 3]);
 });
 
-app.listen(3000, () => console.log('Listening on port 3000...'));
+const port = process.env.PORT;
+
+app.listen(port, () => console.log(`Listening on port ${port}...`));
